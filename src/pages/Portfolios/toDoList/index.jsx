5a2b55caa013fd48ce2e4196045c1f991ca4dcef
Alexandr Porubinovskiy <sp-01@i.ua>
2022-08-25 15:14:49 +0300

    added clear button to search line

--- src/pages/Portfolios/toDoList/index.jsx
+++ src/pages/Portfolios/toDoList/index.jsx
@@ -90,6 +90,10 @@ const ToDoList = () => {
             : setTasksFiltered(tasks_isCompleted);
     }
 
+    function clearInputSearch(e){
+        setSearchValue("");
+    }
+
     return (
         <div className={`page ${styles.pageToDoList}`}>
             <article className={styles.article}>
@@ -102,7 +106,26 @@ const ToDoList = () => {
                                 setIsTrashMode(!isTrashMode);
                             }}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>Trash</title><path d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/><path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32" d="M80 112h352"/><path d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <title>Trash</title>
+                                <path
+                                    d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="32"
+                                />
+                                <path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32" d="M80 112h352" />
+                                <path
+                                    d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="32"
+                                />
+                            </svg>
                         </button>
                         {!isTrashMode && (
                             <button className={styles.addTasksButton} onClick={addTasksFromRemote}>
@@ -132,13 +155,21 @@ const ToDoList = () => {
                             <option value="completed">COMPLETED</option>
                             <option value="not completed">NOT COMPLETED</option>
                         </select>
-                        <input
-                            className={styles.inputSearch + (isTrashMode ? ` ${styles.trashMode}` : "")}
-                            placeholder="search"
-                            onChange={(e) => {
-                                setSearchValue(e.target.value);
-                            }}
-                        />
+                        <div className={styles.filters + " " + styles.filterSearch}>
+                            <input
+                                className={styles.inputSearch + (isTrashMode ? ` ${styles.trashMode}` : "")}
+                                placeholder="search"
+                                value={searchValue}
+                                onChange={(e) => {
+                                    setSearchValue(e.target.value);
+                                }}
+                            >
+                            </input>
+                            <button className={styles.buttonClear} 
+                                onClick = {clearInputSearch}
+                            >
+                            </button>
+                        </div>
                     </div>
                 </section>
 
